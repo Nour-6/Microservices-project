cb6fc9fbea5b1281b272cdd640a726b6e41c93fd
nour-6 <nbenahmed66@gmail.com>
2024-07-13 14:41:23 +0100

    configuring sso Key Clock with Open Feign

--- ms-ticket/src/main/java/com/example/msticket/ticket/EventClient.java
+++ ms-ticket/src/main/java/com/example/msticket/ticket/EventClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("MS-EVENT")
+@FeignClient(name = "MS-EVENT", configuration = FeignConfig.class)
 public interface EventClient {
 
     @GetMapping("/events/{id}")
